56ad89801a6ec50e4141985e8c007aca6275d04d
leandro-si <leandro.almeida@uvvnet.com.br>
2025-03-21 06:36:14 -0300

    criando hook de autenticação

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,27 @@
+import { 
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile,
+    signOut
+ } from "firebase/auth";
+
+ import { useState, useEffect } from "react";
+
+
+ export const useAuthentication = () => {
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    // cleanup
+    // deal with memory leak
+    const [cancelled, setCancelled] = useState(null)
+
+    const auth = getAuth()
+
+    function  checkIfIsCancelled() {
+        if (cancelled) {
+            return
+        }
+    }
+ }
