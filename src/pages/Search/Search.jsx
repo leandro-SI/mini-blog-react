a1274944184bffbca89008d4a31a3413a0b6c007
leandro-si <leandro.almeida@uvvnet.com.br>
2025-03-27 06:48:24 -0300

    exibindo resuldados da busca

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -5,15 +5,32 @@ import React from 'react'
 import { useFetchDocuments } from "../../hooks/useFetchDocument";
 import { useQuery } from "../../hooks/useQuery";
 
+// components
+import PostDetails from '../../components/PostDetails';
+
+import { Link } from "react-router-dom";
+
 const Search = () => {
 
     const query = useQuery()
     const search = query.get("q")
 
+    const { documents: posts } = useFetchDocuments("posts", search)
+
     return (
-        <div>
+        <div className={styles.search_container}>
             <h2>Search</h2>
-            <p>{search}</p>
+            <div>
+                {posts && posts.length == 0 && (
+                    <div className={styles.no_posts}>
+                        <p>Não foram encontrados posts a partir de sua bsuca.</p>
+                        <Link to="/" className='btn btn-dark'>Voltar</Link>
+                    </div>
+                )}
+                {posts && posts.map((post) => (
+                    <PostDetails key={post.id} post={post} />
+                ))}
+            </div>
         </div>
     )
 }
