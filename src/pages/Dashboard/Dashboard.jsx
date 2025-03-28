280aad2eddf350edbddc52628cd7ed86918a1672
leandro-si <leandro.almeida@uvvnet.com.br>
2025-03-28 07:15:16 -0300

    excluindo um post

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 // hooks
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 const Dashboard = () => {
 
@@ -13,10 +14,8 @@ const Dashboard = () => {
     const uid = user.uid
     const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
 
+    const { deleteDocument } = useDeleteDocument("posts")
 
-    const deleteDocument = (id) => {
-
-    }
 
     if (loading)
         return <p>Carregando...</p>
